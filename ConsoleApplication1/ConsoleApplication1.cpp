6afd3a3b815b7e1db429b243b75b1b522f11e517
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-09-06 11:02:03 +0300

    First milestone

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,27 @@
 #include <stdio.h>
+
 int main() {
-	printf("Hello, World!\n");
-	return 0;
+    int command;
+    printf("You can use next commands: \n");
+    printf("1 - Text append\n");
+    printf("2 - New line start\n");
+    printf("3 - Save the text to the file\n");
+    printf("4 - Load the text from the file\n");
+    printf("5 - Current text print\n");
+    printf("6 - Insert the text by line and symbol index\n");
+    printf("7 - Text search\n");
+    printf("8 - Text clear\n");
+
+    while (true)
+    {
+        printf("Enter the number of command: ");
+        scanf_s("%d", &command);
+
+        if (command != 0)
+        {
+            printf("The command is not implemented\n");
+        }
+    }
+
+    return 0;
 }
