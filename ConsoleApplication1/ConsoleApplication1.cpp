7d4ac41fa5d1856baac887ff623153f0439ada4c
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-09-14 12:18:13 +0300

    Commands 1 5

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
     int command;
+    char** textArray = NULL;
+    int numRows = 0;
+    int currentRow = 0;
+    const int maxLineLength = 100;
+
     printf("You can use next commands: \n");
     printf("1 - Text append\n");
     printf("2 - New line start\n");
@@ -12,14 +19,36 @@ int main() {
     printf("7 - Text search\n");
     printf("8 - Text clear\n");
 
-    while (true)
+    while (1)
     {
         printf("Enter the number of command: ");
         scanf_s("%d", &command);
+        getchar();
 
-        if (command != 0)
+        if (command == 1)
         {
-            printf("The command is not implemented\n");
+            char input[maxLineLength];
+            printf("Enter the text: ");
+            scanf_s("%s", input, sizeof(input));
+
+            if (currentRow == 0) {
+                textArray = (char**)realloc(textArray, (currentRow + 1) * sizeof(char*));
+                textArray[currentRow] = (char*)malloc((strlen(input) + 1) * sizeof(char));
+                strcpy_s(textArray[currentRow], strlen(input) + 1, input);
+                currentRow++;
+            }
+            else {
+                textArray[currentRow - 1] = (char*)realloc(textArray[currentRow - 1], (strlen(textArray[currentRow - 1]) + strlen(input) + 1) * sizeof(char));
+                strcat_s(textArray[currentRow - 1], strlen(textArray[currentRow - 1]) + strlen(input) + 1, input);
+            }
+        }
+
+        else if (command == 5)
+        {
+            for (int i = 0; i < currentRow; i++)
+            {
+                printf("%s\n", textArray[i]);
+            }
         }
     }
 
